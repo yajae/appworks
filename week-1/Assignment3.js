6b6e6791173b3d80822f2fca025243addc1fd05e
yajie <puu882@gmail.com>
2024-04-12 12:30:13 +0800

    modify package.json / script

---
+++ week-1/Assignment3.js
@@ -0,0 +1,37 @@
+function countAandB(input) {
+    let count=0;
+    for (let i=0;i<input.length;i++)
+    {if(input[i]==="a"|input[i]==="b"){count++}};
+    return count;
+}
+
+
+function toNumber(input) {
+    let arr=[];
+    for(let i=0;i<input.length;i++){
+        switch(input[i]){
+            case 'a':
+                arr.push(1);break;
+            case 'b':
+                arr.push(2);break;
+            case 'c':
+                arr.push(3);break;
+            case 'd':
+                arr.push(4);break;
+            default:
+                arr.push(5);
+        }
+        
+    }
+    return arr;
+}
+
+//I finished above 
+
+
+let input1 = ['a', 'b', 'c', 'a', 'c', 'a', 'c'];
+console.log(countAandB(input1)); // should print 4 (3 ‘a’ letters and 1 ‘b’ letter)
+console.log(toNumber(input1)); // should print [1, 2, 3, 1, 3, 1,3]
+let input2 = ['e', 'd', 'c', 'd', 'e'];
+console.log(countAandB(input2)); // should print 0
+console.log(toNumber(input2)); // should print [5, 4, 3, 4, 5]
