84884001fcafd491fd8958bb76c3c907b3e1e0c6
yajie <puu882@gmail.com>
2024-04-14 20:03:23 +0800

    modify the route that is wrong when the number<1

--- Week-3/Assignment/Assignment5.js
+++ Week-3/Assignment/Assignment5.js
@@ -7,4 +7,6 @@ function twoSum(nums, target) {
         }
     }
 }
+
+
 // twoSum([2, 7, 11, 15], 9);
--- Week-3/Assignment/routes/index.js
+++ Week-3/Assignment/routes/index.js
@@ -5,7 +5,7 @@ router.get("/",(req,res) => {
     let number=req.query.number;
     if(!number) {
         res.send("Lack of Parameter!");
-    }else if(isNaN(Number(number))) {
+    }else if(isNaN(Number(number))||Number(number)<1) {
         res.send("Wrong Parameter");
     }else{
         let result = 0;
